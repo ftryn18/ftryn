f73050015f59509c425b71c4231ce6252e67f99d
ftryn18 <fitri18yani10@gmail.com>
2024-10-24 20:29:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('SELAMAT HARI LAHIR IPEH SAYANGG, HADIAH KU BUATMU:v').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
